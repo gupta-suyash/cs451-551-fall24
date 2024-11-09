2f79900327fca8754129e371b9b061346ff84533
Steven Walton <walton.stevenj@gmail.com>
2024-11-08 17:13:30 -0800

    Updating __main__ to functions
    Leaving the original code in we return db, grades_table, query, and keys
    from main and test with this loop
    ```python
    uc = (0, [1,1,1,1,1],1)
    for key in keys:
        // our new query from main
        t0 = function_query.select_version(key, *uc)[0].columns
        // our original query
        t1 = query.select_version(key, *uc)[0].columns
        if t0 != t1: print(f"==> Key {key} failed with {t0} != {t1}")
    ```

--- __main__.py
+++ __main__.py
@@ -1,56 +1,184 @@
+from collections.abs import Callable
+from typing import Optional
 from lstore.db import Database
 from lstore.query import Query
 from time import process_time
-from random import choice, randrange
+import random
 
-# Student Id and 4 grades
-db = Database()
-grades_table = db.create_table('Grades', 5, 0)
-query = Query(grades_table)
-keys = []
+def timeit(message   : str,
+           f         : Callable,
+           query     : Callable,
+           *args,
+           tab_width : int = 40,
+           **kwargs,
+           ):
+    """ 
+    Time the function 
+    Args:
+        message: Message to display to user
+        f: function to time
+        query: query created from db (grades_table)
+        *args: arguments to pass to the function
+        tab_width: integer for tab width post message
+        **kwargs: keyword arguments to function
 
-insert_time_0 = process_time()
-for i in range(0, 10000):
-    query.insert(906659671 + i, 93, 0, 0, 0)
-    keys.append(906659671 + i)
-insert_time_1 = process_time()
+    Returns:
+        rval: return value from function f
+    """
+    start_time = process_time()
+    rval = f(query, *args, **kwargs)
+    end_time = process_time()
+    print(f"{message + " took:":<{tab_width}} {(end_time - start_time):0.6f} seconds")
+    return rval
 
-print("Inserting 10k records took:  \t\t\t", insert_time_1 - insert_time_0)
+def trivial_query(query   : Callable,
+                  keys    : list[int],
+                  n       : int       = 10000,
+                  offset  : int       = 906659671,
+                  columns : list[int] = [93,0,0,0],
+                  ) -> Callable:
+    """
+    Trivial query function where we insert some dummy columns
+    Args:
+        query: the query object
+        keys: Our list of keys
+        n: number of rounds
+        offset: some random offset seed
+        columns: a constant dummy column
 
-# Measuring update Performance
-update_cols = [
-    [None, None, None, None, None],
-    [None, randrange(0, 100), None, None, None],
-    [None, None, randrange(0, 100), None, None],
-    [None, None, None, randrange(0, 100), None],
-    [None, None, None, None, randrange(0, 100)],
-]
+    Returns:
+        keys: list of updated keys
+    """
+    for i in range(n):
+        query.insert(offset+i, *columns)
+        keys.append(offset+i)
+    return keys
 
-update_time_0 = process_time()
-for i in range(0, 10000):
-    query.update(choice(keys), *(choice(update_cols)))
-update_time_1 = process_time()
-print("Updating 10k records took:  \t\t\t", update_time_1 - update_time_0)
+def trivial_update(query   : Callable,
+                   n       : int                 = 10000,
+                   keys    : list[int]           = [],
+                   columns : Optional[list[int]] = None,
+                   ) -> None:
+    """
+    Update the queries 
+    Args:
+        query: query object
+        n: number of rounds 
+        keys: list of keys
+        columns: our input columns
 
-# Measuring Select Performance
-select_time_0 = process_time()
-for i in range(0, 10000):
-    query.select(choice(keys),0 , [1, 1, 1, 1, 1])
-select_time_1 = process_time()
-print("Selecting 10k records took:  \t\t\t", select_time_1 - select_time_0)
+    Returns:
+        None
+    """
+    for _ in range(n):
+        query.update(random.choice(keys), *(choice(columns)))
+    return 
 
-# Measuring Aggregate Performance
-agg_time_0 = process_time()
-for i in range(0, 10000, 100):
-    start_value = 906659671 + i
-    end_value = start_value + 100
-    result = query.sum(start_value, end_value - 1, randrange(0, 5))
-agg_time_1 = process_time()
-print("Aggregate 10k of 100 record batch took:\t", agg_time_1 - agg_time_0)
+def trivial_select(query   : Callable,
+                   n       : int           = 10000,
+                   keys    : list[int]     = [],
+                   columns : Optional[list[int]] = None,
+                   ) -> None:
+    """
+    Testing select function by randomly selecting keys
+    Args:
+        query: query object
+        n: number of rounds 
+        keys: list of keys
+        columns: our input columns
 
-# Measuring Delete Performance
-delete_time_0 = process_time()
-for i in range(0, 10000):
-    query.delete(906659671 + i)
-delete_time_1 = process_time()
-print("Deleting 10k records took:  \t\t\t", delete_time_1 - delete_time_0)
+    Returns:
+        None
+    """
+    for _ in range(n):
+        query.select(random.choice(keys), *columns)
+    return 
+
+def aggregate(query  : Callable,
+              n      : int = 10000,
+              batch  : int = 100,
+              offset : int = 906659671,
+              ) -> None:
+    """
+    Aggregate checking function
+    Tests the query.sum function
+
+    Args:
+        query: query object
+        n: number of rounds 
+        batch: batch size we work over
+        offset: a seed offset
+    Returns:
+        None
+    """
+    for i in range(0,n,batch):
+        _val = offset + i
+        _end = _val + batch
+        result = query.sum(_val, _end-1, random.randrange(0,5))
+    return 
+
+def delete_records(query     : Callable,
+                   n         : int = 10000,
+                   offset    : int = 906659671,
+                   ) -> None:
+    """
+    Tests deleting queries
+
+    Args:
+        query: query object
+        n: number of rounds
+        offset: some random offset seed
+
+    Returns:
+        None
+    """
+    for i in range(n):
+        query.delete(offset+i)
+    return 
+
+def main(n           : int = 10000,     # Number of iterations
+         offset      : int = 906659671, # Random seed
+         table_name  : str = "Grades",  # name of our table
+         num_columns : int = 5,         # Number of 'grades'
+         key_index   : int = 0,         # Our primary key
+         ) -> None:
+    """
+    Our main testing function
+    We will test different aspects of the program through here
+    """
+    columns = [93, 0, 0, 0]
+
+    db = Database()
+    grades_table = db.create_table(name, num_columns, key_index)
+    query = Query(grades_table)
+    keys = []
+
+    keys = timeit(f"Inserting {n} records", trivial_query, 
+                         query=query, n=n,
+                         keys=keys, offset=offset, columns=columns) 
+
+    # Create a dummy 5x5 matrix of columns
+    # First is all Nones, then diagonal is randrange(0,100)
+    N = 5
+    update_cols = [ [None for _ in range(N)] for _ in range(N) ]
+    for i in range(1,N):
+        update_cols[i][i] = random.randrange(0,100)
+
+    timeit("Updating 10k records", 
+           trivial_update, query=query, n=n,
+           keys=keys, columns=update_cols)
+
+    sel_cols = (0, [1,1,1,1,1])
+    timeit("Selecting 10k records", 
+           trivial_select, query=query, n=n,
+           keys=keys, columns=sel_cols)
+
+    timeit("Aggregate 10k of 100 records batch", 
+           aggregate, query=query, n=n,
+           )
+    timeit("Deleting 10k records", 
+           delete_records, query=query, n=n,
+           )
+
+if __name__ == '__main__':
+    main()
